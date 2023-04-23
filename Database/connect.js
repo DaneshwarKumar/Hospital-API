ce4e4bae6f9e41a3cf560bd409b0aba925a012c8
Daneshwar Kumar <Daneshwarkumar218@gmail.com>
2023-04-24 00:19:15 +0530

    Hospital API

--- Database/connect.js
+++ Database/connect.js
@@ -1,10 +1,9 @@
 
 
-// connecting nodejs with mongodb 
+// connecting nodejs with mongodb using mongoose 
 const mongoose = require("mongoose");
 
 
-// connecting with mongodb with mongoose 
 mongoose.connect('mongodb://127.0.0.1:27017/Hospital-API')
 .then(() => console.log("Mongodb Connected successfully"))
 .catch((err) => console.log("not Connected"));
--- Router/route.js
+++ Router/route.js
@@ -3,9 +3,70 @@ const express = require('express');
 const router = express.Router();
 const Doctor = require("../Models/doctor");
 const Patient = require("../Models/patient");
+const jwt = require('jsonwebtoken');
+const secretKey = "HospitalApikey";
 
 
-// sending the data to doctor
+// creating home page section
+router.get('/' , (req,res) => {
+    res.send("<h1> Welcome to Hospital API  </h1>");
+});
+
+
+
+// authenticating the doctor 
+router.post("doctor/login" , (req,res) => {
+    const user = {
+        id : 1,
+        userName : "Daneshwar Kumar",
+        password : "Daneshwar@123"
+    }
+    jwt.sign({user} , secretKey , {expiresIn:'500s'} , (err,token) => {
+        res.json({
+            token
+        })
+    })
+});
+
+
+// generating the tokens 
+router.post("/profile" , varifyToken, (req,res) => {
+   
+    jwt.varify(req.token , secretKey , (err,authData) => {
+        if(err){
+            res.send({result : "Invalid Token"});
+        }
+        else{
+            res.json({
+                message : "profile Access Successfully",
+                authData
+            })
+        }
+    })
+    
+});
+
+// function for generating and varifyng the tokens 
+function varifyToken(req,res,next){
+    const bearerHeader = req.headers['authorization'];
+
+    if(typeof bearerHeader !== 'undefined'){
+       const bearer = bearerHeader.split(" ");
+       const token = bearer[1];
+       req.token = token;
+       next();
+    }else{
+        res.send({
+            result : 'Token is Not Valid'
+        })
+    }
+}
+
+
+
+
+
+// adding the new doctor record 
 router.post("/doctor/register" , async(req,res) => {
     try{
         const doctor = new Doctor(req.body);
@@ -17,19 +78,19 @@ router.post("/doctor/register" , async(req,res) => {
 
 
 
-// sending the data to patient
+// adding the new patient record 
 router.post("/patient/register" , async(req,res) => {
     try{
-        req.body.doctor = '644266a8fa92fe7458393c4c';
-        const doctor = new Patient(req.body);
-        const createDoctor = await(doctor.save());
-        res.status(201).send(createDoctor);
+        req.body.doctor = '64456edc443ae8e4d3f904cd';
+        const patient = new Patient(req.body);
+        const createPatient = await(patient.save());
+        res.status(201).send(createPatient);
     }catch(e){res.status(400).send(e)}
 });
 
 
 
-// getting the report 
+// adding the patient report by id 
 router.post("/patient/report/:id" , async(req,res) => {
     try{
         const patient = await Patient.findById(req.params.id);
